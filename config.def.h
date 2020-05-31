6d400a49a1eb631100e4d297c2b3cb75e1590746
alpha-tango-kilo <atk@aaathats3as.com>
2020-05-31 09:59:45 +0100

    Applied patches/dwm-vanitygaps-20190508-6.2.diff

--- config.def.h
+++ config.def.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 6;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 6;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 6;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 6;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMonoMedium Nerd Font:pixelsize=20:antialias=true:autohint=true" };
@@ -92,6 +97,9 @@ static Key keys[] = {
 	{ MODKEY,                XK_m,              setlayout,      {.v = &layouts[2]} }, /* monocle */
 	{ MODKEY|ShiftMask,      XK_space,          togglefloating, {0} },
 
+    { MODKEY,			     XK_z,		        incrgaps,	    {.i = +3 } },
+    { MODKEY,			     XK_x,		        incrgaps,	    {.i = -3 } },
+
 	{ MODKEY,                XK_space,          zoom,           {0} },
 	{ MODKEY,                XK_backslash,      view,           {0} },
 	{ MODKEY,                XK_0,              view,           {.ui = ~0 } },
@@ -100,6 +108,24 @@ static Key keys[] = {
 	{ MODKEY,                XK_Right,          focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,      XK_Left,           tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,      XK_Right,          tagmon,         {.i = +1 } },
+
+    /* All the gap bindings holy cow
+    { MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
+	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },*/
 };
 
 /* button definitions */
